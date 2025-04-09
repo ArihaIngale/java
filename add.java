fbec45669a19ed7571d119febd7524d11e6c7e8c
ArihaIngale <arihaingale220705@gmail.com>
2025-04-09 14:25:18 +0530

    we r adding

--- add.java
+++ add.java
@@ -1,12 +1,11 @@
 package java;
-
 public class add {
         public static void main(String[] args) {
             int a = 5;
             int b = 10;
-            int sum = a + b;
+            int c = 15;
+            int sum = a + b + c;
             System.out.println("The sum is: " + sum);
         }
     }
-
 
